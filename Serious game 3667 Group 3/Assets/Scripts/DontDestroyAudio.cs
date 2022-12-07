6c9d90b6c2cb9506eb5e79c2cb0879081d775b2f
David Weng <davidweng12345@gmail.com>
2022-12-07 03:02:15 -0500

    Menu added with Instruction and Settings

--- Serious game 3667 Group 3/Assets/Scripts/DontDestroyAudio.cs
+++ Serious game 3667 Group 3/Assets/Scripts/DontDestroyAudio.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class DontDestroyAudio : MonoBehaviour
 {
-   void Awake()
-   {
-        DontDestroyOnLoad(transform.gameObject);
-   }
+    public static DontDestroyAudio instance;
+ 
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
 }
+ 
