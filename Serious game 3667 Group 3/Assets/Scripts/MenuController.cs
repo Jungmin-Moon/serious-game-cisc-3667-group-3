f8d6ec45bffd7bb18f0459b0bdb330d03fa3c44f
David Weng <davidweng12345@gmail.com>
2022-12-06 21:25:34 -0500

    Small changes to david branch

---
+++ Serious game 3667 Group 3/Assets/Scripts/MenuController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string mainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            } else 
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;   
+    }
+
+    public void ReturnToMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
